67eda7f5afc62a56ff3f81cd39932d74e9ff33aa
salmonkarp <garyaxelmuliyono@gmail.com>
2025-08-19 18:34:45 +0800

    Initial commit: Rename, Greet, Exit
    Added simple behaviour to greet the user.

---
+++ src/main/java/DogBot.java
@@ -0,0 +1,14 @@
+public class DogBot {
+    public static void main(String[] args) {
+        String greetingMessage = """
+                ____________________________________________________________
+                 Hello! I'm DogBot
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                                """;
+        ;
+        System.out.println(greetingMessage);
+    }
+}
