ae7d2aeaffea1d9f836520ad461f3b5e93d2c647
salmonkarp <garyaxelmuliyono@gmail.com>
2025-08-20 13:20:05 +0800

    Add echo and bye behaviour

--- src/main/java/DogBot.java
+++ src/main/java/DogBot.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class DogBot {
-    public static void main(String[] args) {
+
+    public static Scanner textScanner = new Scanner(System.in);
+
+    public static String addEnclosingLines(String input) {
+        return """
+                ____________________________________________________________
+                """ + input + "\n" + """
+                ____________________________________________________________
+                """;
+    }
+
+    public static void printInitialMessage() {
         String greetingMessage = """
-                ____________________________________________________________
-                 Hello! I'm DogBot.
-                 What can I do for you?
-                ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                ____________________________________________________________
-                                """;
+                Hello! I'm Dogbot.
+                What can I do for you?
+                               """;
         ;
-        System.out.println(greetingMessage);
+        System.out.println(addEnclosingLines(greetingMessage));
+    }
+
+    public static void main(String[] args) {
+        printInitialMessage();
+        String userInput = "";
+        while (true) {
+            System.out.print("Insert your input here: ");
+            userInput = textScanner.nextLine();
+            if (userInput.toLowerCase().equals("bye"))
+                break;
+            System.out.println(addEnclosingLines(userInput));
+
+        }
+        System.out.println(addEnclosingLines("Bye. Hope to see you again soon!"));
     }
 }
